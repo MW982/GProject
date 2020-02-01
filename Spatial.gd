33aba597e8c49f9b82420d1bad4f210fbdfbad2c
razor9876 <kamil.sienkiewicz1997@gmail.com>
2020-02-01 19:44:07 +0100

    grenades now dealing dmg and emitting light when exploding

--- Spatial.gd
+++ Spatial.gd
@@ -6,19 +6,21 @@ const GRENADE_DAMAGE = 60
 const GRENADE_TIME = 2
 var grenade_timer = 0
 
-const EXPLOSION_WAIT_TIME = 0.48
+const EXPLOSION_WAIT_TIME = 0.98
 var explosion_wait_timer = 0
 
 var rigid_shape
 var grenade_mesh
 var blast_area
 var explosion_particles
+var light
 
 func _ready():
     rigid_shape = $Collision_Shape
     grenade_mesh = $Grenade
     blast_area = $Blast_Area
     explosion_particles = $Explosion
+    light = $OmniLight
 
     explosion_particles.emitting = false
     explosion_particles.one_shot = true
@@ -31,6 +33,7 @@ func _process(delta):
     else:
         if explosion_wait_timer <= 0:
             explosion_particles.emitting = true
+            light.light_energy = 1
 
             grenade_mesh.visible = false
             rigid_shape.disabled = true
@@ -39,13 +42,18 @@ func _process(delta):
 
             var bodies = blast_area.get_overlapping_bodies()
             for body in bodies:
-                if body.has_method("bullet_hit"):
-                    body.bullet_hit(GRENADE_DAMAGE, body.global_transform.looking_at(global_transform.origin, Vector3(0, 1, 0)))
+                if body.has_method("damage"):
+                    print(body)
+                    body.damage(GRENADE_DAMAGE)
 
             # This would be the perfect place to play a sound!
 
 
         if explosion_wait_timer < EXPLOSION_WAIT_TIME:
+            if explosion_wait_timer < EXPLOSION_WAIT_TIME/2:
+                 light.light_energy += 0.5
+            else:
+                 light.light_energy -= 0.7
             explosion_wait_timer += delta
 
             if explosion_wait_timer >= EXPLOSION_WAIT_TIME:
